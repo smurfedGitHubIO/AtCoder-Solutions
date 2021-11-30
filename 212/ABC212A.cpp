5678154df3e5998e52fd3236e17b8cdb67e0d06a
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2021-11-30 22:42:00 +0800

    Add files via upload

---
+++ 212/ABC212A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a, b;
+	cin >> a >> b;
+	if(a == 0)
+		cout << "Silver";
+	else if(b == 0)
+		cout << "Gold";
+	else
+		cout << "Alloy";
+	return 0;
+}
