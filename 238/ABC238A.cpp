9c3bd75a3a5acb72a2cc327dc3e28c25cbf6979e
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-02-11 21:48:30 +0800

    Add files via upload

---
+++ 238/ABC238A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+	ll n;
+	cin >> n;
+	if(n <= 4 and n >=2){
+		cout << "No";
+	}
+	else{
+		cout << "Yes";
+	}
+	return 0;
+}
