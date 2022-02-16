8a3f3a2f8dbc444837162a0308a639c38608b8e0
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2022-02-16 14:39:45 +0800

    Add files via upload

---
+++ 218/ABC218A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+	ll n;
+	cin >> n;
+	string s;
+	cin >> s;
+	cout << (s[n-1] == 'o' ? "Yes" : "No");
+	return 0;
+}
