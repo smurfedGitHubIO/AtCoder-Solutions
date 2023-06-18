f2043456bb9143faba63f355a9e69a47943b6580
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-06-18 19:24:09 +0800

    feat: solve abc306a

---
+++ 306/ABC306A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+  ll n;
+  cin >> n;
+  string s;
+  cin >> s;
+  for(ll i=0; i<n; i++){
+    cout << s[i] << s[i];
+  }
+  return 0;
+}
