b81fdeec7bbe39b52bc4f1c9abcc729b82198b40
Justin Clyde V. Frongoso <clydefrongoso@gmail.com>
2023-10-25 08:55:03 +0800

    feat: add abc323a

---
+++ 300+/323/ABC323A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+int main(){
+    string s;
+    cin >> s;
+    for(ll i=0; i<16; i++){
+        if(s[i]&1 and s[i] == '1'){
+            cout << "No";
+            return 0;
+        }
+    }
+    cout << "Yes";
+    return 0;
+}
